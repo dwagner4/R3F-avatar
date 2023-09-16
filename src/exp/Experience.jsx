ed6a0785f1682fc82a767375bbf5937d606c9e94
Dean Wagner <dean@dfwagners.net>
2023-09-16 22:34:06 +0000

    added additional animations

--- src/exp/Experience.jsx
+++ src/exp/Experience.jsx
@@ -1,15 +1,22 @@
 import { OrbitControls } from "@react-three/drei";
 import { Cube } from '../models/Cube.jsx'
 import { Avatar } from '../models/avatar/Avatar.jsx'
+import { useControls } from "leva";
 
 export const Experience = () => {
+  const {animation} = useControls({
+    animation: {
+      value: "Typing",
+      options: ["Typing", "Falling", "Standing"]
+    }
+  })
   return (
     <>
       <OrbitControls />
       <Cube position={[ -1, 0, 1]} tag="Joe" fsm="shapeRef1" friend="shapeRef2"/>
       <Cube position={[ 1, 0, -1]} tag = "fart" fsm="shapeRef2" friend="shapeRef1"/>
       <group position-y={-1}>
-        <Avatar />
+        <Avatar animation={animation} />
       </group>
       <ambientLight intensity={1} />
     </>
